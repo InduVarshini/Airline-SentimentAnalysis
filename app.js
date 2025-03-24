819ec1c79871d83d4d594c2459249b2e91c157c3
Indu Varshini Jayapal <induvarshinij2000@gmail.com>
2025-03-24 13:09:07 -0600

    Update app.js

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ if (!vizContainer) {
 const options = {
     hideTabs: true,
     hideToolbar: false,
+    device: "desktop",
     onFirstInteractive: function() {
         debug('Dashboard is now interactive');
         vizLoaded = true;
